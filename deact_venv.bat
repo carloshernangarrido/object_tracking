f959f850d851a316cd108381458fb8251f68e35a
Hernán <carloshernangarrido@gmail.com>
2022-09-16 10:19:23 -0300

    emd working with mask frequencies

--- deact_venv.bat
+++ deact_venv.bat
@@ -1,4 +1,3 @@
-cd venv/Scripts
+cd C:\TRABAJO\CONICET\venvs\object_tracking_venv\Scripts
 CALL deactivate
-cd ..
-cd ..
+cd C:\Users\joses\Mi unidad\TRABAJO\46_cm_inerter\TRABAJO\experimental\ensayos\object_tracking
